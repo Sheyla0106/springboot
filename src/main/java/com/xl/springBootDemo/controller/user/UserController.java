8f247767c6110f6b56a81e3ba1b0a57eb199e310
sheyla0106 <zxl_zsg#163.com>
2019-06-20 00:22:20 +0800

    集成log4j2
    集成log4j2

--- src/main/java/com/xl/springBootDemo/controller/user/UserController.java
+++ src/main/java/com/xl/springBootDemo/controller/user/UserController.java
@@ -2,6 +2,8 @@ package com.xl.springBootDemo.controller.user;
 
 import com.xl.springBootDemo.bean.user.UserBean;
 import com.xl.springBootDemo.service.user.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,14 +20,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("user")
 public class UserController {
 
+    public static final Logger logger = LoggerFactory.getLogger(UserController.class);
     @Autowired
     private UserService userService;
 
     @GetMapping("/{id}")
     public UserBean gerUserById(@PathVariable Long id) {
+        logger.info("info請求參數為：{}",id);
         UserBean userBean = new UserBean();
+        logger.error("error請求參數為：{}",id);
 
         userBean = userService.getUserById(id);
+        logger.debug("debug請求參數為：{}",id);
+
+
         return userBean;
     }
 }
